c169a56e0972df5c396627c5a328e778fc70c682
OthmanEmpire <oz.alikhan@gmail.com>
2017-03-07 14:26:25 +0000

    Refactored 'logUser' which now only tracks first join and final voice leave

--- friends/main.py
+++ friends/main.py
@@ -9,17 +9,20 @@ ID = "285129311487524864"
 TOKEN = "Mjg1MTI5MzExNDg3NTI0ODY0.C5NsmQ.G30YlYDzQnmmgaI-KHg_vAtQIdc"
 INVITE = "https://discordapp.com/oauth2/authorize?client_id=285129311487524864&scope=bot&permissions=0"
 
-# client = discord.Client()
+dataDir = os.path.join(".", "data")
+logPath = os.path.join(dataDir, "log.csv")
+plotPath = os.path.join(dataDir, "history.svg")
+
 description = '''And so it begins...'''
 bot = commands.Bot(command_prefix='OE ', description=description)
+
 
 @bot.event
 @asyncio.coroutine
 def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+    initialiseDirs()
+    createUserLogFile(header=["TIME", "JOIN", "USER"], delimiter=',')
+    print('Logged in as: {}-{}\n'.format(bot.user.name, bot.user.id))
 
 
 @bot.event
@@ -40,49 +43,63 @@ def on_message(message):
 @asyncio.coroutine
 def on_voice_state_update(before, after):
 
-    dataDir = os.path.join(".", "data")
-    logPath = os.path.join(dataDir, "log.csv")
-    plotPath = os.path.join(dataDir, "history.svg")
-
-    if not os.path.exists(dataDir):
-        os.makedirs(dataDir)
-
-    logUser(after, logPath, delimiter=",")
+    logUser(before, after, logPath, delimiter=",")
     # plotUsers(logPath, plotPath)
 
 
-def logUser(member, filePath, delimiter):
+def logUser(before, after, filePath, delimiter):
     """
     Writes the user activity (joining or leaving) a voice channel into a file.
+    Swapping channels is not recorded.
 
     The format of a data entry in the csv file contains the following in order:
         - Time (datetime)
         - IsConnect (boolean)
         - User (string)
 
-    :param member: Discord Member object.
+    :param before: Discord Member object.
+    :param after: Discord Member object.
     :param filePath: Output file path.
     :param delimiter: String delimiter used in the CSV output file.
     """
-    name = member.name
+    name = after.name
+    isJoin = None
     time = datetime.datetime.now()
-    if member.voice.voice_channel:
-        isConnect = True
-    else:
-        isConnect = False
 
-    header = ["TIME", "JOIN", "USER"]
-    entry = [time, isConnect, name]
+    # User joins a voice channel for the first time
+    if not before.voice.voice_channel and after.voice.voice_channel:
+        isJoin = True
 
-    if not os.path.exists(filePath):
-        with open(filePath, mode="w", newline='') as f:
-            csvWriter = csv.writer(f, delimiter=delimiter)
-            csvWriter.writerow(header)
-    else:
+    # User leaves all voice channels
+    if not after.voice.voice_channel:
+        isJoin = False
+
+    if isJoin is not None:
+        entry = [time, isJoin, name]
         with open(filePath, mode="a", newline='') as f:
             csvWriter = csv.writer(f, delimiter=delimiter)
             csvWriter.writerow(entry)
 
+
+def initialiseDirs():
+    """
+    Creates all necessary directories that the bot uses.
+    """
+    if not os.path.exists(dataDir):
+        os.makedirs(dataDir)
+
+
+def createUserLogFile(header, delimiter):
+    """
+    Creates the file that stores user voice joining and leaving activity.
+
+    :param header: List of Strings.
+    :param delimiter: String.
+    """
+    if not os.path.exists(logPath):
+        with open(logPath, mode="w", newline='') as f:
+            csvWriter = csv.writer(f, delimiter=delimiter)
+            csvWriter.writerow(header)
 
 
 
@@ -103,14 +120,16 @@ def logUser(member, filePath, delimiter):
     #         yield from bot.send_message(channel, msg, tts=True)
 
 
-
-bot.run(TOKEN)
+if __name__ == "__main__":
+    bot.run(TOKEN)
 
 
 # BLUEPRINT
 ###########
 #
 # 1. Logs and graphs members activity
+#    1.1 Graph per day user activity and cumulative activity (straight lines vs time)
+#    1.2 Weekly Bar graph for total hours per day
 #
 # 2. Add puns function
 #
@@ -119,3 +138,10 @@ bot.run(TOKEN)
 # 4. Play "I'll be back" when someone leaves a voice channel
 #
 # 5. Accompanies lone person in voice channel and does creepy TTS conversation
+#
+# 6. Bot sends text messages to call other bots to join channel
+
+
+# TODO:
+# 1. Perhaps use class instead?
+# 2. Move the code below the header to somewhere more appropriate
